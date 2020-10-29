78259566ea40e38448ed1f786661c416b53ded61
Bernardo Aires <bernardoaires@terra.com.br>
2020-10-28 23:45:42 -0300

    feat wiki search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+interface ResultParams {
+  title: string,
+  snippet: string,
+  pageid: number
+}
 
 export const Search: React.FC = () => {
+  const [term, setTerm] = useState<string>('')
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      })
+
+      setResults(data.query.search)
+    }
+
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search()
+      }
+    }, 500)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [term])
+
+  const renderedResults = results.map((result: ResultParams) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+        </div>
+        <div className="content">
+          <div className="header">
+            {result.title}
+          </div>
+          <span dangerouslySetInnerHTML={{ __html:result.snippet }}></span>
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div>
-      Search
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input onChange={(e) => setTerm(e.target.value)} value={term} className="input" />
+        </div>
+      </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   )
 }
